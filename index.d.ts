94eecbf2a62fecb6e9ceeae284c217d751a570a6
liangjingxian <liangjingxian@zuzuche.com>
2021-07-19 13:48:47 +0800

    style: 移除注释的代码

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,6 @@ import { Consul, ConsulOptions, Agent } from "consul";
 interface EggConsulOptions {
   server?: ConsulOptions;
   client?: any;
-  // client?: Agent.Service.RegisterOptions;
 }
 
 declare module 'egg' {
